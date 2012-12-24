2198df6cb513e217aaa412b8a4e9c5c17657a2fe
Cammy_the_block <awesomepossumcraft@gmail.com>
2012-12-24 09:21:54 -0800

    Added checking for invalid moves and added proper placemnt of moves.

--- gangup.java
+++ gangup.java
@@ -3,41 +3,77 @@ package TB_Game;
 import java.util.Scanner;
 
 public class gangup {
-	
-	public static void startGangup(){
-		
+
+	public static void startGangup() {
+		int playerNumber = 1;
 		int[][] board = new int[8][8];
 		System.out.print("Welcome to TB Game's Gangup. ");
 		board = clearGangupBoard(board);
-		printGangupBoard(board);
+		gangupTurn(board, playerNumber);
 	}
-	public static int[][] clearGangupBoard(int[][] board){
-		
-		for (int x = 0; x <= 7; x++){
-			for (int y = 0; y <= 7; y++){
+
+	public static int[][] clearGangupBoard(int[][] board) {
+
+		for (int x = 0; x <= 7; x++) {
+			for (int y = 0; y <= 7; y++) {
 				board[x][y] = 0;
 			}
 		}
 		return board;
 	}
-	public static void printGangupBoard(int[][] board){
-		
-		for (int x = 0; x <= 7; x++){
+
+	public static void printGangupBoard(int[][] board) {
+
+		for (int x = 7; x >= 0; x--) {
 			System.out.println();
-			System.out.print(7 - x + " ");
-			for (int y = 0; y <= 7; y++){
+			System.out.print(x + " ");
+			for (int y = 0; y <= 7; y++) {
 				System.out.print(board[x][y] + " ");
 			}
 		}
 		System.out.print("\n" + "  0 1 2 3 4 5 6 7");
 	}
-	public static void gangupTurn(){
-		Scanner scannerC = new Scanner(System.in);
-		System.out.print("Where do you want to go [XY]?");
-		String input = scannerC.next();
-		char x = input.charAt(1);
-		char y = input.charAt(2);
+
+	public static void gangupTurn(int[][] board, int playerNumber) {
+		System.out.print("\n" + "Player #" + playerNumber);
+		printGangupBoard(board);
+		Scanner scannerInt = new Scanner(System.in);
+		System.out.print("\n" + "Where do you want to go?" + "\n" + "X:");
+		int inputX = scannerInt.nextInt();
+		System.out.print("Y:");
+		int inputY = scannerInt.nextInt();
+		if ((inputX < 8 && inputX >= 0) && (inputY < 8 && inputY >= 0)){
+			if (board[inputY][inputX]== 0){
+				board[inputY][inputX] = playerNumber;
+				boolean win = checkForWin();
+				if (win == true){
+					
+				}
+				else{
+					if (playerNumber < 4){
+						playerNumber++;
+						gangupTurn(board, playerNumber);
+					}
+				}
+			}
+			else{
+				invalidMove(board, playerNumber);
+			}
+		}
+		else{
+			invalidMove(board, playerNumber);
+		}
+
+	}
+	private static void invalidMove(int[][] board, int playerNumber) {
+		System.out.print("Invalid Move");
+		gangupTurn(board, playerNumber);
 		
 	}
+
+	public static boolean checkForWin(){
+		return false;
+	}
+	
 
 }
