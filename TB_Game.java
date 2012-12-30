c8693faf902175ed1c17048bffb90eee3d035c21
Cammy_the_block <awesomepossumcraft@gmail.com>
2012-12-29 21:09:23 -0800

    Added crypting

--- TB_Game.java
+++ TB_Game.java
@@ -10,14 +10,26 @@ public class TB_Game {
 				|| whatToDo.equalsIgnoreCase("1")) {
 			gangup.startGangup();
 		}
+		if (whatToDo.equalsIgnoreCase("Encryption")
+				|| whatToDo.equalsIgnoreCase("3")) {
+			cryption.encrypt();
+		}
+		if (whatToDo.equalsIgnoreCase("Decryption")
+				|| whatToDo.equalsIgnoreCase("4")) {
+			cryption.decrypt();
+		}
 	}
+
 	public static String menu() {
-		
+
 		String whatToDo;
 		Scanner scannerS = new Scanner(System.in);
 		System.out.print("Welcome to the TextBased Game! " + "\n" + "\n"
-				+ "Which game do you wish to play?" + "\n" + "1 - Gangup"
-				+ "\n" + "2 - Black Jack - Coming Soon");
+				+ "Which game do you wish to play?" + "\n"
+				+ "1 - Gangup - Work in Progress" + "\n"
+				+ "2 - Black Jack - Coming Soon" + "\n"
+				+ "3 - Encryption - Work in Progress" + "\n"
+				+ "4 - Decryption - Work in Progress");
 		System.out.println();
 		whatToDo = scannerS.nextLine();
 		return whatToDo;
