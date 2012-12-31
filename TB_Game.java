39beb0e6fb1157f34a6d56f451b1fc42863bd19d
Cammy_the_block <awesomepossumcraft@gmail.com>
2012-12-30 16:52:05 -0800

    Improved menu features.

--- TB_Game.java
+++ TB_Game.java
@@ -5,7 +5,21 @@ import java.util.Scanner;
 public class TB_Game {
 
 	public static void main(String[] args) {
-		String whatToDo = menu();
+		menu();
+	}
+
+	public static void menu() {
+
+		String whatToDo;
+		Scanner scannerS = new Scanner(System.in);
+		System.out.print("Welcome to the TextBased Game! " + "\n" + "\n"
+				+ "Which game do you wish to play?" + "\n"
+				+ "1 - Gangup - Work in Progress" + "\n"
+				+ "2 - Black Jack - Coming Soon" + "\n"
+				+ "3 - Encryption - Work in Progress" + "\n"
+				+ "4 - Decryption - Work in Progress");
+		System.out.println();
+		whatToDo = scannerS.nextLine();
 		if (whatToDo.equalsIgnoreCase("Gangup")
 				|| whatToDo.equalsIgnoreCase("1")) {
 			gangup.startGangup();
@@ -18,20 +32,9 @@ public class TB_Game {
 				|| whatToDo.equalsIgnoreCase("4")) {
 			cryption.decrypt();
 		}
-	}
-
-	public static String menu() {
-
-		String whatToDo;
-		Scanner scannerS = new Scanner(System.in);
-		System.out.print("Welcome to the TextBased Game! " + "\n" + "\n"
-				+ "Which game do you wish to play?" + "\n"
-				+ "1 - Gangup - Work in Progress" + "\n"
-				+ "2 - Black Jack - Coming Soon" + "\n"
-				+ "3 - Encryption - Work in Progress" + "\n"
-				+ "4 - Decryption - Work in Progress");
-		System.out.println();
-		whatToDo = scannerS.nextLine();
-		return whatToDo;
+		else {
+			System.out.println("Invalid Option");
+			menu();
+		}
 	}
 }
