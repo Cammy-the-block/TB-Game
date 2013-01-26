62732099ad70093b64ac2be365b9c20fa90be6ef
Cammy_the_block <awesomepossumcraft@gmail.com>
2013-01-26 10:41:01 -0800

    Added GUI features

--- TB_Game.java
+++ TB_Game.java
@@ -1,6 +1,7 @@
 package TB_Game;
 
 import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class TB_Game {
 
@@ -11,15 +12,15 @@ public class TB_Game {
 	public static void menu() {
 
 		String whatToDo;
-		Scanner scannerS = new Scanner(System.in);
-		System.out.print("Welcome to the TextBased Game! " + "\n" + "\n"
+		whatToDo = JOptionPane.showInputDialog("Welcome to the TextBased Game! " + "\n" + "\n"
 				+ "Which game do you wish to play?" + "\n"
 				+ "1 - Gangup - Work in Progress" + "\n"
-				+ "2 - Black Jack - Coming Soon" + "\n"
-				+ "3 - Encryption - Work in Progress" + "\n"
-				+ "4 - Decryption - Work in Progress");
-		System.out.println();
-		whatToDo = scannerS.nextLine();
+				+ "2 - Black Jack - Coming Soon" + "\n" 
+				+ "3 - Encryption - Non GUI" + "\n"
+				+ "4 - Decryption - Non GUI" + "\n"
+				+ "5 - Encryption GUI" + "\n"
+				+ "6 - Decryption GUI" + "\n"
+				+ "7 - Exit");
 		if (whatToDo.equalsIgnoreCase("Gangup")
 				|| whatToDo.equalsIgnoreCase("1")) {
 			gangup.startGangup();
@@ -32,9 +33,33 @@ public class TB_Game {
 				|| whatToDo.equalsIgnoreCase("4")) {
 			cryption.decrypt();
 		}
+		if (whatToDo.equalsIgnoreCase("Encryption GUI")
+				|| whatToDo.equalsIgnoreCase("5")) {
+			cryptionGUI.encrypt();
+		}
+		if (whatToDo.equalsIgnoreCase("Decryption GUI")
+				|| whatToDo.equalsIgnoreCase("6")) {
+			cryptionGUI.decrypt();
+		}
+		if (whatToDo.equalsIgnoreCase("Exit")
+				|| whatToDo.equalsIgnoreCase("7")) {
+			exit();
+		}
 		else {
-			System.out.println("Invalid Option");
+			JOptionPane.showMessageDialog(null, "Invalid Option", "Error Message", JOptionPane.PLAIN_MESSAGE);
 			menu();
 		}
-	}
+		}
+		public static void exit() {
+			int quit;
+			quit = JOptionPane.showConfirmDialog(null, "Do you wish to close TB Game?");
+			if (quit == 0) {
+				JOptionPane.showMessageDialog(null, "Bye!", "TB Game", JOptionPane.PLAIN_MESSAGE);
+				System.exit(0); 
+			}
+			else {
+				menu();
+			}
+		}
+	
 }
