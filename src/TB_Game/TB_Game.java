3196b3b673c0355aca47875c1e0928f5c6f545af
Cammy_the_block <awesomepossumcraft@gmail.com>
2013-11-14 20:20:27 -0800

    Added null checking and replaced ifs with switch.

--- src/TB_Game/TB_Game.java
+++ src/TB_Game/TB_Game.java
@@ -1,7 +1,10 @@
 package TB_Game;
 
 import java.util.Scanner;
+
 import javax.swing.JOptionPane;
+
+import org.apache.commons.lang3.text.WordUtils;
 
 public class TB_Game {
 
@@ -11,59 +14,64 @@ public class TB_Game {
 
 	public static void menu() {
 
-		String whatToDo;
-		try{
-		whatToDo = JOptionPane
+		String whatToDo = JOptionPane
 				.showInputDialog("Welcome to the TextBased Game! " + "\n"
 						+ "\n" + "Which game do you wish to play?" + "\n"
 						+ "1 - Gangup - Work in Progress" + "\n"
-						+ "2 - Black Jack - Coming Soon" + "\n"
+						+ "2 - Black Jack - Not Been Finished" + "\n"
 						+ "3 - Encryption - Non GUI" + "\n"
 						+ "4 - Decryption - Non GUI" + "\n"
 						+ "5 - Encryption GUI" + "\n" + "6 - Decryption GUI"
 						+ "\n" + "7 - Alchemy - Work In Progress" + "\n"
 						+ "8 - Exit");
-		if (whatToDo.equalsIgnoreCase("Gangup")
-				|| whatToDo.equalsIgnoreCase("1")) {
-			gangup.startGangup();
-		}
-		if (whatToDo.equalsIgnoreCase("Black Jack")
-				|| whatToDo.equalsIgnoreCase("2")) {
-			gangup.startGangup();
-		}
-		if (whatToDo.equalsIgnoreCase("Encryption")
-				|| whatToDo.equalsIgnoreCase("3")) {
-			cryption.encrypt();
-		}
-		if (whatToDo.equalsIgnoreCase("Decryption")
-				|| whatToDo.equalsIgnoreCase("4")) {
-			cryption.decrypt();
-		}
-		if (whatToDo.equalsIgnoreCase("Encryption GUI")
-				|| whatToDo.equalsIgnoreCase("5")) {
-			cryptionGUI.encrypt();
-		}
-		if (whatToDo.equalsIgnoreCase("Decryption GUI")
-				|| whatToDo.equalsIgnoreCase("6")) {
-			cryptionGUI.decrypt();
-		}
-		if (whatToDo.equalsIgnoreCase("Alchemy")
-				|| whatToDo.equalsIgnoreCase("7")) {
-			Alchemy.startAlchemy();
-		}
-		if (whatToDo.equalsIgnoreCase("Exit") || whatToDo.equalsIgnoreCase("8")) {
-			exit();
-		} else {
+		if (whatToDo == null) {
 			JOptionPane.showMessageDialog(null, "Invalid Option",
 					"Text Based Game", JOptionPane.PLAIN_MESSAGE);
 			menu();
 		}
-		}
-		catch(Exception e){
-			JOptionPane.showMessageDialog(null, "Invalid Option",
-					"Text Based Game", JOptionPane.PLAIN_MESSAGE);
-			menu();
-		}
+		whatToDo = whatToDo.trim();
+		
+		whatToDo = WordUtils.capitalize(whatToDo);
+        switch (whatToDo.toUpperCase()){
+        	case "Gangup":
+        	case "1":
+        		gangup.startGangup();
+        		break;
+        	case "Black Jack":
+        	case "2":
+        		JOptionPane.showMessageDialog(null, "Black Jack has not been finished", "Text Based Game",
+    					JOptionPane.PLAIN_MESSAGE);
+    			menu();
+    			break;
+        	case "Encryption":
+        	case "3":
+        		cryption.encrypt();
+        		break;
+        	case "Decryption":
+        	case "4":
+        		cryption.decrypt();
+        		break;
+        	case "Encryption GUI":
+        	case "5":
+        		cryptionGUI.encrypt();
+        		break;
+        	case "Decryption GUI":
+        	case "6":
+        		cryptionGUI.decrypt();
+        	case "Alchemy":
+        	case "7":
+        		Alchemy.startAlchemy();
+        		break;
+        	case "Exit":
+        	case "8":
+        		exit();
+        	default:
+        		JOptionPane.showMessageDialog(null, "Invalid Option", "Text Based Game", JOptionPane.PLAIN_MESSAGE);
+    			menu();
+    			
+        }
+      
+		
 	}
 
 	public static void exit() {
