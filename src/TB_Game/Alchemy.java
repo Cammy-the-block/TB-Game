91c616f57fbaca383d7808b978e569794c4a00ca
Cammy_the_block <awesomepossumcraft@gmail.com>
2014-01-19 18:50:33 -0800

    Moved TextFileToArray to utils.

--- src/TB_Game/Alchemy.java
+++ src/TB_Game/Alchemy.java
@@ -4,6 +4,8 @@ import java.util.Scanner;
 
 import javax.swing.JOptionPane;
 
+import utils.TextFileToArray;
+
 public class Alchemy {
 	
 	
--- src/utils/TextFileToArray.java
+++ src/utils/TextFileToArray.java
@@ -1,4 +1,4 @@
-package TB_Game;
+package utils;
 
 import java.io.File;
 import java.util.Scanner;
